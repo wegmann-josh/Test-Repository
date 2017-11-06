01ae1b410ec56024b30fbf5dfaacf031a08df4ec
jwegmann4020 <jwegmann4020@shs64910-121.mcboe.madison.k12.al.us>
2017-11-06 07:31:47 -0600

    new commit

--- FinalProject/src/finalproject/FinalProject.java
+++ FinalProject/src/finalproject/FinalProject.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package finalproject;
 
-/**
- *
- * @author jwegmann4020
- */
+import java.util.Random;
+
 public class FinalProject {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
     
 }
